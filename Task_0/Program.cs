d6b5f03d96c4b7f79269673a403c8fa835f8a9f3
Helen <e.xolodkova@inbox.ru>
2022-04-16 21:50:40 +0300

    Добавили Task 0

---
+++ Task_0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Квадрат числа
+int number = -3;
+int numberSquare = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {numberSquare}");
+
+int numberA = 5;
+numberA = numberA * numberA;
+Console.Write("Квадрат равен ");
+Console.Write(numberA);
