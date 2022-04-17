a6751a4943cac4c0a294beb87abbf5fa612c6391
Helen <e.xolodkova@inbox.ru>
2022-04-18 01:09:18 +0300

    Добавили решение задачи №10(кратн. 23 и 7)

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if((a % 23 == 0) & (a % 7 == 0))
+{
+    System.Console.WriteLine($"Число {a} кратно 23 и 7");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} НЕ кратно 23 и 7");
+}
