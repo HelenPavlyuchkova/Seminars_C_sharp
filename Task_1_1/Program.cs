d2b305c4c641b7022ceac80c310d2fdd032a680d
Helen <e.xolodkova@inbox.ru>
2022-04-18 01:02:40 +0300

    Добавили решение задачи №1 семинара 2

---
+++ Task_1_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// найти вторую цифру трехзначного числа
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = number % 100;
+number = number / 10;
+System.Console.WriteLine(number);
+
