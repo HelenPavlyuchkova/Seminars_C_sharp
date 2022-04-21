8d27b8ae122fd0339124684e4b84af2168c7ea4b
Helen <e.xolodkova@inbox.ru>
2022-04-21 23:49:18 +0300

    Добавили задачу 14(таблица квадратов)

---
+++ Task_14/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N
+int N = 12;
+int i = 1;
+while(i < N)
+{
+    System.Console.Write($"{i*i}, ");
+    i++;
+}
+System.Console.WriteLine($"{i*i}.");
