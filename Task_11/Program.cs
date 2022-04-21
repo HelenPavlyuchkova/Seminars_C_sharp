6001df449f46d1dd4d62cb0e57e7e7dbe3484c9e
Helen <e.xolodkova@inbox.ru>
2022-04-21 23:46:53 +0300

    Добавили задачу 11(точки в плоскости)

---
+++ Task_11/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+Console.Clear();
+int x = new Random().Next(-1000, 1001);
+System.Console.WriteLine($" x = {x}");
+int y = new Random().Next(-1000, 1001);
+System.Console.WriteLine($" y = {y}");
+
+// Console.WriteLine("Введите координату х: ");
+// int x = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите координату y: ");
+// int y = int.Parse(Console.ReadLine());
+if(x ==0 | y == 0)
+{
+    System.Console.WriteLine("Точка не принадлежит ни одной из координатных плоскостей");
+}
+else if(x > 0 & y > 0)
+{
+    System.Console.WriteLine("Точка в 1й четверти плоскости");
+}
+else if(x < 0 & y > 0)
+{
+    System.Console.WriteLine("Точка в 2й четверти плоскости");
+}
+else if(x < 0 & y < 0)
+{
+    System.Console.WriteLine("Точка в 3й четверти плоскости");
+}
+else
+{
+    System.Console.WriteLine("Точка в 4й четверти плоскости");
+}
