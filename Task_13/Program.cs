b44e2c600614a7e81987a866665aa658eaac27c7
Helen <e.xolodkova@inbox.ru>
2022-04-21 23:48:49 +0300

    Добавили задачу 13(длина отрезка)

---
+++ Task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+int x1, x2, y1, y2;
+
+x1 = 7;
+y1 = -5;
+x2 = 10; 
+y2 = -1;
+int AB = (int)Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)); //кастомизировали int в int
+//double AB = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+System.Console.WriteLine(AB);
