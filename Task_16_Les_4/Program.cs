abd1882e2ad00c3a7a912fc65ff266ce1ef7c427
Helen <e.xolodkova@inbox.ru>
2022-05-01 22:51:40 +0300

    Добавили решение задачи 26(сем.4) о кол-ве цифр в числе

---
+++ Task_16_Les_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+while (num > 0)
+{
+    num = num / 10;
+    count++;
+}
+System.Console.WriteLine(count);
+
+//второй способ
+System.Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine(a.ToString().Length);
