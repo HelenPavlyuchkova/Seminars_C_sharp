de7dbf96f42a056540aa3dd890664bd58d2b5c9f
Helen <e.xolodkova@inbox.ru>
2022-05-01 22:52:44 +0300

    Добавили решение задачи 28(сем.4) о произведении чисел от 1 до N

---
+++ Task_17_Les_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N, а выдает произведение чисел от 1 до N
+System.Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int mult = 1;
+for (int i = 1; i <= N; i++)
+{
+    System.Console.Write(i + " ");
+    mult = mult * i;
+}
+System.Console.WriteLine();
+System.Console.Write(mult);
