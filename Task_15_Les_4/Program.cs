decb73e4b9f7834905631d105a2eddd7d7c031c9
Helen <e.xolodkova@inbox.ru>
2022-05-01 22:50:35 +0300

    Добавили решение задачи 24(сем.4) о сумме чисел от 1 до А

---
+++ Task_15_Les_4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 24. Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+System.Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    System.Console.Write(i + " ");
+    sum = sum + i;
+}
+// System.Console.WriteLine(sum);
+System.Console.WriteLine();
+
+//через цикл while
+int j = 1;
+while(j <= A)
+{
+    System.Console.Write(j + " ");
+    // sum += j;
+    j++;
+}
+System.Console.WriteLine(sum);
+
+//через метод
+int SumOfNum(int N)
+{
+    int sum1 = 0;
+    for (int i = 1; i<= N; i++)
+    {
+        sum1 += i;
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+    return sum1;
+}
+int s = SumOfNum(A);
+System.Console.WriteLine(s);
