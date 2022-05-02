7d1c66984a588bdfca841817847359ca4c4e1310
Helen <e.xolodkova@inbox.ru>
2022-05-02 23:15:20 +0300

    Добавили задачу 31 (сем.5) сумма положит и отриц эл.массива

---
+++ Task_19_Les_5/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму 
+// отрицательных и положительных элементов массива
+void FillArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random().Next(-9, 10);
+    }
+}
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        System.Console.Write(col[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] array = new int[12];
+FillArray(array);
+PrintArray(array);
+
+int sumPos = 0;
+int sumNeg = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sumPos = sumPos + array[i];
+    }
+    else
+    {
+        sumNeg = sumNeg + array[i];
+    }
+}
+System.Console.WriteLine($"Сумма положительных чисел: {sumPos}, и сумма отрицательных чисел: {sumNeg}");
+
