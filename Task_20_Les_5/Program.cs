aeea71db2a62509154bccc2394071effc4fbc7a2
Helen <e.xolodkova@inbox.ru>
2022-05-02 23:16:17 +0300

    Добавили задачу 32 (сем.5) замена положит и отриц эл.массива

---
+++ Task_20_Les_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на 
+// соответствующие отрицательные, и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+void FillArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random().Next(-9, 10);
+    }
+}
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        System.Console.Write(col[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = - array[i];
+}
+PrintArray(array);
