964c6e2d01a3c61d08f1d226fbfe36258a5d9fd9
Helen <e.xolodkova@inbox.ru>
2022-05-04 00:39:29 +0300

    Добавили решение задачи №42(сем.6) перевод дес числа в двоичное

---
+++ Task_26_Les_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101 3 -> 11 2 -> 10
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+string s = String.Empty;
+//int A = 0;
+while (N > 0)
+{
+    int A = N % 2;
+    s = A.ToString() + s;//строка, в которой наши остатки накапливаются, как sum =sum+A, но меняем местами слагаемые
+    N = N / 2;
+}
+System.Console.WriteLine(s);
