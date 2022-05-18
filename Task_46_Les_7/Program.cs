8d47fffdbdda258bafca633e3a2f55f8a8e4c84a
Helen <e.xolodkova@inbox.ru>
2022-05-18 23:43:48 +0300

    Добавили решение задачи 46 зад.двумерного массива целых

---
+++ Task_46_Les_7/Program.cs
@@ -0,0 +1,27 @@
+﻿// 46.  Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int m = 5;
+int n = 4;
+
+int[,] mass = new int [m,n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0;j < mass.GetLength(1);j++)
+    {
+        mass[i,j] = new Random().Next(-10,10);
+        System.Console.Write(mass[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
+//дополнительно
+System.Console.WriteLine();
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1);j++)
+    {
+        
+        mass[i,j] = mass[i,j] * mass[i,j];
+        System.Console.Write(mass[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
