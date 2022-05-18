ba602def8ae71750ec3e79a21e0794243d05f543
Helen <e.xolodkova@inbox.ru>
2022-05-18 23:47:31 +0300

    Добавили решение задачи 48 кажд.эл массива как сумма индексов

---
+++ Task_48_Les_7/Program.cs
@@ -0,0 +1,22 @@
+﻿// 48. Задайте двумерный массив размера m на n,каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран. m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+System.Console.WriteLine("Введите количество строк m");
+int m = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine("Введите количество столбцов n");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] mass = new int [m,n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j] = i+j;
+        System.Console.Write(mass[i,j] + " ");
+    }
+System.Console.WriteLine();
+}
