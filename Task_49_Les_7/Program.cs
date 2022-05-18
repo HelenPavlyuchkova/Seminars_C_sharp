857e35804352cc6037c55aed42f7ac8fe1fc8c0c
Helen <e.xolodkova@inbox.ru>
2022-05-18 23:44:57 +0300

    Добавили решение задачи 49 замена эл. с четн индексами на квадраты

---
+++ Task_49_Les_7/Program.cs
@@ -0,0 +1,31 @@
+﻿// 49. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и 
+// замените эти элементы на их квадраты.
+int m = 3;
+int n = 4;
+int[,] mass = new int[m,n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0;j < mass.GetLength(1);j++)
+    {
+        mass[i,j] = new Random().Next(0,10);
+        System.Console.Write(mass[i,j] + " ");
+    }
+System.Console.WriteLine();
+}
+System.Console.WriteLine();
+for (int i = 0; i < mass.GetLength(0); i=i+2)//шаг=2, обходим только четные индексы
+{
+    for (int j = 0; j < mass.GetLength(1); j=j+2)
+    {
+        mass[i,j] = mass[i,j]*mass[i,j];
+    }
+}
+System.Console.WriteLine("Получили массив: "); //нумерация с нуля
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0;j < mass.GetLength(1);j++)
+    {
+        System.Console.Write($"{mass[i,j]} ");
+    }
+System.Console.WriteLine();
+}
