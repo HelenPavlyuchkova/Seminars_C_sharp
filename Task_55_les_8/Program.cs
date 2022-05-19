896c4c84c0de0c568af851196064e6e3d5d0ea2d
Helen <e.xolodkova@inbox.ru>
2022-05-19 22:49:27 +0300

    задача 55 о смене местами первой строки двумерного массива на последнюю

---
+++ Task_55_les_8/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, 
+// если это невозможно, программа должна вывести сообщение для пользователя.
+
+int m = 5;
+int n = 3;
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " "); ;
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+
+if(m != n)
+{
+    System.Console.WriteLine("найти решение задачи невозможно");
+    return;
+}
+
+int temp = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = i; j < array.GetLength(1); j++)
+    {
+        temp = array[i, j];
+        array[i, j] = array[j, i];
+        array[j, i] = temp;
+    }
+}
+PrintArray(array);
+
+//на примере 1й строки
+// int temp = 0;
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         temp = array[0,j];
+//         array[0,j] = array[j,0];
+//         array[j,0] = temp;
+//     }
+// }
+// PrintArray(array);
