d88bc13b4f6746a160ce7d5a5a286966dc7172a6
Helen <e.xolodkova@inbox.ru>
2022-05-22 00:12:09 +0300

    задача 67 о сумме цифр через рекурсию

---
+++ Task_67_Les_9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12  45 -> 9
+
+int Rec(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    // System.Console.WriteLine(number % 10 + " ");
+    //int summa = Rec(number / 10);
+    System.Console.WriteLine(number % 10 + " ");//вместо 11 строки
+    return Rec(number / 10) + number % 10;
+}
+int sum = Rec(1234);
+System.Console.WriteLine(sum);
